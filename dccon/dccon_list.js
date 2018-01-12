cd714d2093a1eff37d2c5125c3c90bdc413e063c
krynen <krynen2530@gmail.com>
2018-01-13 03:14:32 +0900

    mod 1con

--- dccon/dccon_list.js
+++ dccon/dccon_list.js
@@ -9,7 +9,7 @@
 	{"name":"역가흠.png",			"keywords":["역가흠", "여까흠"],				"tags":["역가"]},
 	{"name":"역가서순.png",			"keywords":["서순"],						"tags":["역가"]},
 	{"name":"역가순서.png",			"keywords":["순서"],						"tags":["역가"]},
-	{"name":"역헤가오.png",			"keywords":["아흑", "v", "V"],					"tags":["역가"]},
+	{"name":"역헤가오.png",			"keywords":["아흑", "vv", "VV"],					"tags":["역가"]},
 	
 	{"name":"역가골든무말모.gif",		"keywords":["골든무말모", "고올든무말모"],			"tags":["역가", "움짤"]},
 	
